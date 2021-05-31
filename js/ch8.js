6a80db68eba160bdf555876a5bbffefe6b50dd83
Josh Dorn <jr_dorn@yahoo.com>
2021-05-31 16:41:49 -0500

    EJ Ch8 Exercises

--- js/ch8.js
+++ js/ch8.js
@@ -177,3 +177,72 @@ function firstElement(array) {
   return array[0];
 }
 // firstElement([]);
+
+//Exercises
+
+//Retry
+
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Klunk");
+  }
+}
+function reliableMultiply(a, b) {
+  try {
+    return primitiveMultiply(a, b);
+  } catch (e) {
+    if (e instanceof MultiplicatorUnitFailure) {
+      return reliableMultiply(a, b);
+    } else {
+      throw e;
+    }
+  }
+}
+// console.log(reliableMultiply(8, 8));
+
+//The Locked Box
+
+const box = {
+  locked: true,
+  unlock() {
+    this.locked = false;
+  },
+  lock() {
+    this.locked = true;
+  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  },
+};
+
+function withBoxUnlocked(functionVal) {
+  let wasUnlocked = box.locked;
+  if (!wasUnlocked) {
+    return functionVal();
+  }
+  box.unlock();
+  try {
+    return functionVal();
+  } finally {
+    box.lock();
+  }
+}
+
+withBoxUnlocked(function () {
+  box.content.push("dubloons");
+});
+
+try {
+  withBoxUnlocked(function () {
+    throw new Error("There be pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised: " + e);
+}
+console.log(box.locked); //true
