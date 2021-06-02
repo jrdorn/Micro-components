25fe34866c25e9a03cf992527d85d38db82fd73a
Josh Dorn <jr_dorn@yahoo.com>
2021-06-02 15:36:01 -0500

    EJ Ch12 Exercises

--- js/ch12.js
+++ js/ch12.js
@@ -189,3 +189,44 @@ specialForms.fun = (args, scope) => {
 };
 
 //Exercises
+
+//Arrays
+topScope.array = (...values) => values;
+
+topScope.length = (array) => array.length;
+
+topScope.element = (array, i) => array[i];
+
+run(`
+do(define(sum, fun(array,
+     do(define(i, 0),
+        define(sum, 0),
+        while(<(i, length(array)),
+          do(define(sum, +(sum, element(array, i))),
+             define(i, +(i, 1)))),
+        sum))),
+   print(sum(array(1, 2, 3))))
+`);
+
+//Comments
+function skipSpace(string) {
+  let skippable = string.match(/^(\s|#.*)*/);
+  return string.slice(skippable[0].length);
+}
+
+//Fixing Scope
+specialForms.set = (args, env) => {
+  if (args.length != 2 || args[0].type != "word") {
+    throw new SyntaxError("Bad use of set");
+  }
+  let varName = args[0].name;
+  let value = evalutate(args[1], env);
+
+  for (let scope = env; scope; scope = Object.getPrototypeOf(scope)) {
+    if (Object.prototype.hasOwnProperty.call(scope, varName)) {
+      scope[varName] = value;
+      return value;
+    }
+  }
+  throw new ReferenceError(`Setting undefined variable ${varName}`);
+};
