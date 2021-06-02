d6d722665e359c0d92daa81ff33d25ee79dae5e2
Josh Dorn <jr_dorn@yahoo.com>
2021-06-02 11:19:20 -0500

    EJ Ch11 Exercises

--- js/ch11.js
+++ js/ch11.js
@@ -253,3 +253,64 @@ async function chicks(nest, year) {
   });
   return (await Promise.all(lines)).join("\n");
 }
+
+//Exercises
+
+//Tracking the Scalpel
+async function locateScalpel(nest) {
+  let current = nest.name;
+  for (;;) {
+    let next = await anyStorage(nest, current, "scalpel");
+    if (next == current) return current;
+    current = next;
+  }
+}
+function locateScalpel2(nest) {
+  function loop(current) {
+    return anyStorage(nest, current, "scalpel").then((next) => {
+      if (next == current) return current;
+      else return loop(next);
+    });
+  }
+  return loop(nest.name);
+}
+
+//Building Promise.all
+
+function Promise_all(promises) {
+  return new Promise((resolve, reject) => {
+    let results = [];
+    let pending = promises.length;
+    for (let i = 0; i < promises.length; i++) {
+      promisesp[i]
+        .then((result) => {
+          results[i] = result;
+          pending--;
+          if (pending == 0) resolve(results);
+        })
+        .catch(reject);
+    }
+    if (promises.length == 0) resolve(results);
+  });
+}
+
+// Promise.all([]).then((array) => {
+//   console.log("This should be []:", array);
+// });
+// function soon(val) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve(val), Math.random() * 500);
+//   });
+// }
+// Promise_all([soon(1), soon(2), soon(3)]).then((array) => {
+//   console.log("This should be [1, 2, 3]:", array);
+// });
+// Promise_all([soon(1), Promise.reject("X"), soon(3)])
+//   .then((array) => {
+//     console.log("We should not get here");
+//   })
+//   .catch((error) => {
+//     if (error != "X") {
+//       console.log("Unexpected failure:", error);
+//     }
+//   });
