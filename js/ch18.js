616afd85aee6186c02b3b35a321cda4d2082aeb1
Josh Dorn <jr_dorn@yahoo.com>
2021-06-07 09:28:19 -0500

    EJ Ch 18 exercises

--- js/ch18.js
+++ js/ch18.js
@@ -1,5 +1,5 @@
-console.log(encodeURIComponent("Yes?"));
-console.log(decodeURIComponent("Yes%3F"));
+// console.log(encodeURIComponent("Yes?"));
+// console.log(decodeURIComponent("Yes%3F"));
 
 // fetch("example/data.txt").then((response) => {
 //   console.log(response.status);
@@ -18,10 +18,10 @@ console.log(decodeURIComponent("Yes%3F"));
 //   .then((resp) => resp.text())
 //   .then(console.log);
 
-document.querySelector("input").focus();
-console.log(document.activeElement.tagName);
-document.querySelector("input").blur();
-console.log(document.activeElement.tagName);
+// document.querySelector("input").focus();
+// console.log(document.activeElement.tagName);
+// document.querySelector("input").blur();
+// console.log(document.activeElement.tagName);
 
 //The form as a whole
 
@@ -47,108 +47,249 @@ console.log(document.activeElement.tagName);
 //   field.selectionEnd = from + word.length;
 // }
 
-let text = document.querySelector("input");
-let output = document.querySelector("#length");
-text.addEventListener("input", () => {
-  output.textContent = text.value.length;
-});
+// let text = document.querySelector("input");
+// let output = document.querySelector("#length");
+// text.addEventListener("input", () => {
+//   output.textContent = text.value.length;
+// });
 
 //Checkboxes
 
-let checkbox = document.querySelector("#purple");
-checkbox.addEventListener("change", () => {
-  document.body.style.background = checkbox.checked ? "mediumpurple" : "";
-});
+// let checkbox = document.querySelector("#purple");
+// checkbox.addEventListener("change", () => {
+//   document.body.style.background = checkbox.checked ? "mediumpurple" : "";
+// });
 
 //Radio buttons
 
-let buttons = document.querySelectorAll("[name=color]");
-for (let button of Array.from(buttons)) {
-  button.addEventListener("change", () => {
-    document.body.style.background = button.value;
-  });
-}
+// let buttons = document.querySelectorAll("[name=color]");
+// for (let button of Array.from(buttons)) {
+//   button.addEventListener("change", () => {
+//     document.body.style.background = button.value;
+//   });
+// }
 
 //Select fields
 
-let select = document.querySelector("select");
-let out = document.querySelector("#output");
-select.addEventListener("change", () => {
-  let number = 0;
-  for (let option of Array.from(select.options)) {
-    if (option.selected) {
-      number += Number(option.value);
-    }
-  }
-  out.textContent = number;
-});
+// let select = document.querySelector("select");
+// let out = document.querySelector("#output");
+// select.addEventListener("change", () => {
+//   let number = 0;
+//   for (let option of Array.from(select.options)) {
+//     if (option.selected) {
+//       number += Number(option.value);
+//     }
+//   }
+//   out.textContent = number;
+// });
 
 //File fields
 
-function readFileText(file) {
-  return new Promise((resolve, reject) => {
-    let reader = new FileReader();
-    reader.addEventListener("load", () => {
-      //   console.log("File", file.name, "starts with", reader.result.slice(0, 20));
-      resolve(reader.result);
-    });
-    reader.addEventListener("error", () => reject(reader.error));
-    console.log(reader.readAsText(file));
-  });
-}
+// function readFileText(file) {
+//   return new Promise((resolve, reject) => {
+//     let reader = new FileReader();
+//     reader.addEventListener("load", () => {
+//       //   console.log("File", file.name, "starts with", reader.result.slice(0, 20));
+//       resolve(reader.result);
+//     });
+//     reader.addEventListener("error", () => reject(reader.error));
+//     console.log(reader.readAsText(file));
+//   });
+// }
 
-let input = document.querySelector("#file");
-input.addEventListener("change", () => {
-  for (let file of Array.from(input.files)) {
-    readFileText(file);
-  }
-});
+// let input = document.querySelector("#file");
+// input.addEventListener("change", () => {
+//   for (let file of Array.from(input.files)) {
+//     readFileText(file);
+//   }
+// });
 
 //Storing data client-side
 
 localStorage.setItem("username", "PING3");
-console.log(localStorage.getItem("username"));
+// console.log(localStorage.getItem("username"));
 localStorage.removeItem("username");
-console.log(localStorage.getItem("username"));
 
 //Note-taking app
 let list = document.querySelector("select");
 let note = document.querySelector("textarea");
-
 let state;
-function setState(newState) {
-  list.textContent = "";
-  for (let name of Object.keys(newState.notes)) {
-    let option = document.createElement("option");
-    option.textContent = name;
-    if (newState.selected == name) option.selected = true;
-    list.appendChild(option);
-  }
-  note.value = newState.notes[newState.selected];
 
-  localStorage.setItem("Notes", JSON.stringify(newState));
-  state = newState;
-}
-setState(
-  JSON.parse(localStorage.getItem("Notes")) || {
-    notes: { "shopping list": "Carrots\nRaisins" },
-    selected: "shopping list",
-  }
-);
-list.addEventListener("change", () => {
-  setState({ notes: state.notes, selected: list.value });
-});
-note.addEventListener("change", () => {
-  setState({
-    notes: Object.assign({}, state.notes, { [state.selected]: note.value }),
-    selected: state.selected,
+//setState method ensures DOM shows a given state
+//and stores new state to localStorage
+// function setState(newState) {
+//   list.textContent = "";
+//   for (let name of Object.keys(newState.notes)) {
+//     let option = document.createElement("option");
+//     option.textContent = name;
+//     if (newState.selected == name) option.selected = true;
+//     list.appendChild(option);
+//   }
+//   note.value = newState.notes[newState.selected];
+
+//   localStorage.setItem("Notes", JSON.stringify(newState));
+//   state = newState;
+// }
+
+//get starting state from "Notes" value in localStorage
+//or, if empty, create example with only a shopping list in it
+// setState(
+//   JSON.parse(localStorage.getItem("Notes")) || {
+//     //reading nonexistent localStorage field returns null
+//     notes: { "shopping list": "Carrots\nRaisins" },
+//     selected: "shopping list",
+//   }
+// );
+
+// //list of note lists
+// list.addEventListener("change", () => {
+//   //event handler calls setState to move to new state
+//   setState({ notes: state.notes, selected: list.value });
+// });
+
+// //add, remove, or update note items
+// note.addEventListener("change", () => {
+//   //event handler calls setState to move to new state
+//   setState({
+//     notes: Object.assign({}, state.notes, { [state.selected]: note.value }),
+//     selected: state.selected,
+//   });
+// });
+
+// //create a new list
+// document.querySelector("button").addEventListener("click", () => {
+//   let name = prompt("Note name");
+//   if (name)
+//     setState({
+//       notes: Object.assign({}, state.notes, { [name]: "" }),
+//       selected: name,
+//     });
+// });
+
+//Exercises
+
+//Content negotiation
+
+//  text/plain
+//  text/html
+//  application/json
+
+/**
+Use the headers property in the options object 
+passed to fetch to set the header named Accept
+to the desired media type.
+Finally, try asking for the media type application/rainbows+unicorns and
+see which status code that produces.
+
+
+ */
+
+async function logData(url, mediaType) {
+  const response = await fetch(url, {
+    headers: { accept: mediaType },
   });
+  console.log(`${mediaType}: ${await response.text()}`);
+}
+// logData("https://eloquentjavascript.net/author", "text/plain");
+// logData("https://eloquentjavascript.net/author", "text/html");
+// logData("https://eloquentjavascript.net/author", "application/json");
+// logData(
+//   "https://eloquentjavascript.net/author",
+//   "application/rainbows+unicorns"
+// );
+
+//A JavaScript Workbench
+
+document.querySelector("#jsbtn").addEventListener("click", () => {
+  let code = document.querySelector("#jscode").value;
+  let outputNode = document.querySelector("#output");
+  try {
+    let result = Function(code)();
+    outputNode.innerText = String(result);
+  } catch (e) {
+    outputNode.innerText = "Error: " + e;
+  }
 });
-document.querySelector("button").addEventListener("click", () => {
-  let name = prompt("Note name");
-  if (name)
-    setState({
-      notes: Object.assign({}, state.notes, { [name]: "" }),
-      selected: name,
-    });
+
+//Conway's Game of Life
+
+const width = 30,
+  height = 15;
+
+//grid as array of booleans
+
+let gridNode = document.querySelector("#grid");
+//this holds checkboxes that display the grid
+let checkboxes = [];
+for (let y = 0; y < height; y++) {
+  for (let x = 0; x < width; x++) {
+    let box = document.createElement("input");
+    box.type = "checkbox";
+    gridNode.appendChild(box);
+    checkboxes.push(box);
+  }
+  gridNode.appendChild(document.createElement("br"));
+}
+
+function gridFromCheckboxes() {
+  return checkboxes.map((box) => box.checked);
+}
+function checkboxesFromGrid(grid) {
+  grid.forEach((value, i) => (checkboxes[i].checked = value));
+}
+function randomGrid() {
+  let result = [];
+  for (let i = 0; i < width * height; i++) {
+    result.push(Math.random() < 0.3);
+  }
+  return result;
+}
+
+checkboxesFromGrid(randomGrid());
+
+//
+function countNeighbors(grid, x, y) {
+  let count = 0;
+  for (let y1 = Math.max(0, y - 1); y1 <= Math.min(height - 1, y + 1); y1++) {
+    for (let x1 = Math.max(0, x - 1); x1 <= Math.min(width - 1, x + 1); x1++) {
+      if ((x1 != x || y1 != y) && grid[x1 + y1 * width]) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+function nextGeneration(grid) {
+  let newGrid = new Array(width * height);
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      let neighbors = countNeighbors(grid, x, y);
+      let offset = x + y * width;
+      if (neighbors < 2 || neighbors > 3) {
+        newGrid[offset] = false;
+      } else if (neighbors == 2) {
+        newGrid[offset] = grid[offset];
+      } else {
+        newGrid[offset] = true;
+      }
+    }
+  }
+  return newGrid;
+}
+
+function turn() {
+  checkboxesFromGrid(nextGeneration(gridFromCheckboxes()));
+}
+
+document.querySelector("#next").addEventListener("click", turn);
+
+let running = null;
+document.querySelector("#run").addEventListener("click", () => {
+  if (running) {
+    clearInterval(running);
+    running = null;
+  } else {
+    running = setInterval(turn, 400);
+  }
 });
---
+++ js/ch19.js
@@ -0,0 +1,309 @@
+//The State
+
+class Picture {
+  constructor(width, height, pixels) {
+    this.width = width;
+    this.height = height;
+    this.pixels = pixels;
+  }
+  static empty(width, height, color) {
+    let pixels = new Array(width * height).fill(color);
+    return new Picture(width, height, pixels);
+  }
+  pixel(x, y) {
+    return this.pixels[x + y * this.width];
+  }
+  draw(pixels) {
+    let copy = this.pixels.slice();
+    for (let { x, y, color } of pixels) {
+      copy[x + y * this.width] = color;
+    }
+    return new Picture(this.width, this.height, copy);
+  }
+}
+
+function updateState(state, action) {
+  return Object.assign({}, state, action);
+}
+
+//DOM Building
+
+function elt(type, props, ...children) {
+  let dom = document.createElement(type);
+  if (props) Object.assign(dom, props);
+  for (let child of children) {
+    if (typeof child != "string") dom.appendChild(child);
+    else dom.appendChild(document.createTextNode(child));
+  }
+  return dom;
+}
+
+// document.body.appendChild(
+//   elt("button", { onclick: () => console.log("click") }, "The button")
+// );
+
+//The Canvas
+
+const scale = 10;
+
+class PictureCanvas {
+  contructor(picture, pointerDown) {
+    this.dom = elt("canvas", {
+      onmousedown: (event) => this.mouse(event, pointerDown),
+      ontouchstart: (event) => this.touch(event, pointerDown),
+    });
+    this.syncState(picture);
+  }
+  syncState(picture) {
+    if (this.picture == picture) return;
+    this.picture = picture;
+    drawPicture(this.picture, this.dom, scale);
+  }
+}
+
+function drawPicture(picture, canvas, scale) {
+  canvas.width = picture.width * scale;
+  canvas.height = picture.height * scale;
+  let cx = canvas.getContext("2d");
+
+  for (let y = 0; y < picture.height; y++) {
+    for (let x = 0; x < picture.width; x++) {
+      cx.fillStyle = picture.pixel(x, y);
+      cx.fillRect(x * scale, y * scale, scale, scale);
+    }
+  }
+}
+
+PictureCanvas.prototype.mouse = function (downEvent, onDown) {
+  if (downEvent.button != 0) return;
+  let pos = pointerPosition(downEvent, this.dom);
+  let onMove = onDown(pos);
+  if (!onMove) return;
+  let move = (moveEvent) => {
+    if (moveEvent.buttons == 0) {
+      this.dom.removeEventListener("mousemove", move);
+    } else {
+      let newPos = pointerPosition(moveEvent, this.dom);
+      if (newPos.x == pos.x && newPos.y == pos.y) return;
+      pos = newPos;
+      onMove(newPos);
+    }
+  };
+  this.dom.addEventListener("mousemove", move);
+};
+
+function printerPosition(pos, domNode) {
+  let rect = domNode.getBoundingClientRect();
+  return {
+    x: Math.floor((pos.clientX - rect.left) / scale),
+    y: Math.floor((pos.clientY - rect.top) / scale),
+  };
+}
+
+PictureCanvas.prototype.touch = function (startEvent, onDown) {
+  let pos = pointerPosition(startEvent.touches[0], this.dom);
+  let onMove = onDown(pos);
+  startEvent.preventDefault();
+  if (!onMove) return;
+  let move = (moveEvent) => {
+    let newPos = pointerPosition(moveEvent.touches[0], this.dom);
+    if (newPos.x == pos.x && newPos.y == pos.y) return;
+    pos = newPos;
+    onMove(newPos);
+  };
+  let end = () => {
+    this.dom.removeEventListener("touchmove", move);
+    this.dom.removeEventListener("touchend", end);
+  };
+  this.dom.addEventListener("touchmove", move);
+  this.dom.addEventListener("touchend", end);
+};
+
+//The Application
+
+class PixelEditor {
+  constructor(state, config) {
+    let { tools, controls, dispatch } = config;
+    this.state = state;
+
+    this.canvas = new PictureCanvas(state.picture, (pos) => {
+      let tool = tools[this.state.tool];
+      let onMove = tool(pos, this.state, dispatch);
+      if (onMove) return (pos) => onMove(pos, this.state);
+    });
+    this.controls = controls.map((Control) => new Control(state, config));
+    this.dom = elt(
+      "div",
+      {},
+      this.canvas.dom,
+      elt("br"),
+      ...this.controls.reduce((a, c) => a.concat(" ", c.dom), [])
+    );
+  }
+  syncState(state) {
+    this.state = state;
+    this.canvas.syncState(state.picture);
+    for (let ctrl of this.controls) ctrl.syncState(state);
+  }
+}
+
+class ToolSelect {
+  constructor(state, { tools, dispatch }) {
+    this.select = elt(
+      "select",
+      { onchange: () => dispatch({ tool: this.select.value }) },
+      ...Object.keys(tools).map((name) =>
+        elt("option", { selected: name == state.tool }, name)
+      )
+    );
+    this.dom = elt("label", null, "🖌 Tool: ", this.select);
+  }
+  syncState(state) {
+    this.select.value = state.tool;
+  }
+}
+
+//Color picker
+
+class ColorSelect {
+  constructor(state, { dispatch }) {
+    this.input = elt("input", {
+      type: "color",
+      value: state.color,
+      onchange: () => dispatch({ color: this.input.value }),
+    });
+    this.dom = elt("label", null, "🎨 Color: ", this.input);
+  }
+  syncState(state) {
+    this.input.value = state.color;
+  }
+}
+
+//Drawing tools
+
+function draw(pos, state, dispatch) {
+  function drawPixel({ x, y }, state) {
+    let drawn = { x, y, color: state.color };
+    dispatch({ picture: state.picture.draw([drawn]) });
+  }
+  drawPixel(pos, state);
+  return drawPixel;
+}
+
+function rectangle(start, state, dispatch) {
+  function drawRectangle(pos) {
+    let xStart = Math.min(start.x, pos.x);
+    let yStart = Math.min(start.y, pos.y);
+    let xEnd = Math.max(start.x, pos.x);
+    let yEnd = Math.max(start.y, pos.y);
+    let drawn = [];
+    for (let y = yStart; y <= yEnd; y++) {
+      for (let x = xStart; x <= xEnd; x++) {
+        drawn.push({ x, y, color: state.color });
+      }
+    }
+    dispatch({ picture: state.picture.draw(drawn) });
+  }
+  drawRectangle(start);
+  return drawRectangle;
+}
+
+const around = [
+  { dx: -1, dy: 0 },
+  { dx: 1, dy: 0 },
+  { dx: 0, dy: -1 },
+  { dx: 0, dy: 1 },
+];
+
+function fill({ x, y }, state, dispatch) {
+  let targetColor = state.picture.pixel(x, y);
+  let drawn = [{ x, y, color: state.color }];
+  for (let done = 0; done < drawn.length; done++) {
+    for (let { dx, dy } of around) {
+      let x = drawn[done].x + dx,
+        y = drawn[done].y + dy;
+      if (
+        x >= 0 &&
+        x < state.picture.width &&
+        y >= 0 &&
+        y < state.picture.height &&
+        state.picture.pixel(x, y) == targetColor &&
+        !drawn.some((p) => p.x == x && p.y == y)
+      ) {
+        draw.push({ x, y, color: state.color });
+      }
+    }
+  }
+  dispatch({ picture: state.picture.draw(drawn) });
+}
+
+function pick(pos, state, dispatch) {
+  dispatch({ color: state.picture.pixel(pos.x, pos.y) });
+}
+
+//Saving and loading
+
+class SaveButton {
+  contructor(state) {
+    this.picture = state.picture;
+    this.dom = elt(
+      "button",
+      {
+        onclick: () => this.save(),
+      },
+      "💾 Save"
+    );
+  }
+  save() {
+    let canvas = elt("canvas");
+    drawPicture(this.picture, canvas, 1);
+    let link = elt("a", {
+      href: canvas.toDataURL(),
+      download: "pixelart.png",
+    });
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  }
+  syncState(state) {
+    this.picture = state.picture;
+  }
+}
+
+class LoadButton {
+  constructor(_, { dispatch }) {
+    this.dom = elt(
+      "button",
+      {
+        onclick: () => startLoad(dispatch),
+      },
+      "📁 Load"
+    );
+  }
+  syncState() {}
+}
+
+function startLoad(dispatch) {
+  let input = elt("input", {
+    type: "file",
+    onchange: () => finishLoad(input.files[0], dispatch),
+  });
+  document.body.appendChild(input);
+  input.click();
+  input.remove();
+}
+
+function finishLoad(file, dispatch) {
+  if (file == null) return;
+  let reader = new FileReader();
+  reader.addEventListener("load", () => {
+    let image = elt("img", {
+      onload: () =>
+        dispatch({
+          pictureL: pictureFromImage(image),
+        }),
+      src: reader.result,
+    });
+  });
+  reader.readAsDataURL(file);
+}
