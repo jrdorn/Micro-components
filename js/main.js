85702fe59270d292b408d80f3d3ea8993099ee8f
Josh Dorn <jr_dorn@yahoo.com>
2021-06-09 10:30:24 -0500

    Symbols as keys

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("hi");
+let firstName = "Kevin";
+let names = ["Sam", "Pam", "Jam"];
+console.log("My name is ${firstName}");
+console.log(`My name is ${firstName}`); //interpolation
+console.log(`My name is ${names[0]}`);
+
+//Symbols are almost exclusively used as special keys on objects
+
+let meaningOfLife = Symbol("meaningOfLife");
+let hitchhikersGuide = { [meaningOfLife]: 42 };
+console.log(hitchhikersGuide[meaningOfLife]);
