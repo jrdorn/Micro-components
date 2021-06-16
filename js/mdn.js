42350f313bdaface2463a9530d71b15cf6ed6a2c
Josh Dorn <jr_dorn@yahoo.com>
2021-06-16 05:50:07 -0500

    NaN coercion

--- js/mdn.js
+++ js/mdn.js
@@ -12,3 +12,19 @@ console.log(parseInt("010")); //8
 
 //convert binary to integer
 console.log(parseInt("11", 2)); //3
+console.log(parseInt("110", 2)); //6
+
+//parseFloat always uses base 10
+console.log(parseFloat("10.5")); //10.5
+
+//if NaN
+console.log(Number.isNaN(NaN)); //true
+console.log(Number.isNaN("hello")); //false
+
+//global isNaN() function coercion
+console.log(isNaN("hello")); //true
+console.log(isNaN("1")); //false
+console.log(isNaN(undefined)); //true
+console.log(isNaN({})); //true
+console.log(isNaN([1])); //false
+console.log(isNaN([1, 2])); //true
