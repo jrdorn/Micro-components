df7f6c84257193a3bd4baa980e7c7650dfaadc2d
Josh Dorn <jr_dorn@yahoo.com>
2021-06-22 13:35:13 -0500

    DOM api

--- js/main.js
+++ js/main.js
@@ -1,50 +1,13 @@
-//create an AudioContext (cross browser)
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioCtx = new AudioContext();
+const link = document.querySelector("a");
 
-//store references to HTML elements
-const audioElement = document.querySelector("audio");
-const playBtn = document.querySelector("button");
-const volumeSlider = document.querySelector(".volume");
+link.textContent = "Mozilla Developer Network";
 
-//load the audio source into our audio graph
-const audioSource = audioCtx.createMediaElementSource(audioElement);
+link.href = "https://developer.mozilla.org";
 
-//play-pause audio
-playBtn.addEventListener("click", function () {
-  //check if context is in suspended state (autoplay policy)
-  if (audioCtx.state === "suspended") {
-    audioCtx.resume();
-  }
-
-  //if track is stopped, play it
-  if (this.getAttribute("class") === "paused") {
-    audioElement.play();
-    this.setAttribute("class", "playing");
-    this.textContent = "Pause";
-
-    //if track is playing, stop it
-  } else if (this.getAttribute("class") === "playing") {
-    audioElement.pause();
-    this.setAttribute("class", "paused");
-    this.textContent = "Play";
-  }
-});
-
-//if track ends
-audioElement.addEventListener("ended", function () {
-  playBtn.setAttribute("class", "paused");
-  playBtn.textContent = "Play";
-});
-
-//volume
-const gainNode = audioCtx.createGain();
-
-volumeSlider.addEventListener("input", function () {
-  gainNode.gain.value = this.value;
-});
-
-//connect our graph
-audioSource.connect(gainNode).connect(audioCtx.destination);
-
-// Track credit: Outfoxing the Fox by Kevin MacLeod under Creative Commons
+const sect = document.querySelector("section");
+const para = document.createElement("p");
+para.textContent = "We hope you enjoyed the ride.";
+sect.appendChild(para);
+const text = document.createTextNode(" MDN is awesome!");
+const linkPara = document.querySelector("p");
+linkPara.appendChild(text);
