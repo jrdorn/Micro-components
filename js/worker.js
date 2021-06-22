fa63e3c5b09ea4f6ccbcb904012ae6f56bca1f27
Josh Dorn <jr_dorn@yahoo.com>
2021-06-21 19:15:19 -0500

    Web worker setup

---
+++ js/worker.js
@@ -0,0 +1,11 @@
+onmessage = function (e) {
+  console.log("WOrker: message received from main");
+  const result = e.data[0] * e.data[1];
+  if (isNaN(result)) {
+    postMessage("Please write two numbers");
+  } else {
+    const workerResult = `Result: ${result}`;
+    console.log("Worker: posting message back to main script");
+    this.postMessage(workerResult);
+  }
+};
