7c7f420a7ba116d89db545b19720b95372def731
Josh Dorn <jr_dorn@yahoo.com>
2021-06-24 11:52:24 -0500

    WebGL spinning Shrek cube

--- js/main3.js
+++ js/main3.js
@@ -1,1 +1,10 @@
-console.log(scene);
+// import { THREE } from "http://localhost:3000/3.js";
+// const scene = new THREE.Scene();
+//   console.log(scene);
+
+// fetch("http://localhost:3000/3.js")
+//   .then((response) => response.text())
+//   .then((data) => eval(data));
+setTimeout(function () {
+  console.log(7);
+}, 500);
