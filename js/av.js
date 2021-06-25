28972a2f91d5975fcb00e07615532e21931d9ed5
Josh Dorn <jr_dorn@yahoo.com>
2021-06-25 11:53:40 -0500

    AV fonts and functions

--- js/av.js
+++ js/av.js
@@ -1,1 +1,124 @@
-console.log(200);
+const media = document.querySelector("video");
+const controls = document.querySelector(".controls");
+
+const play = document.querySelector(".play");
+const stop = document.querySelector(".stop");
+const rwd = document.querySelector(".rwd");
+const fwd = document.querySelector(".fwd");
+
+const timerWrapper = document.querySelector(".timer");
+const timer = document.querySelector(".timer span");
+const timerBar = document.querySelector(".timer div");
+
+let intervalFwd;
+let intervalRwd;
+
+media.removeAttribute("controls");
+controls.style.visibility = "visible";
+
+function playPauseMedia() {
+  rwd.classList.remove("active");
+  fwd.classList.remove("active");
+  clearInterval(intervalRwd);
+  clearInterval(intervalFwd);
+
+  if (media.paused) {
+    play.setAttribute("data-icon", "u");
+    media.play();
+  } else {
+    play.setAttribute("data-icon", "P");
+    media.pause();
+  }
+}
+
+function stopMedia() {
+  media.pause();
+  media.currentTime = 0;
+
+  rwd.classList.remove("active");
+  fwd.classList.remove("active");
+  clearInterval(intervalRwd);
+  clearInterval(intervalFwd);
+  play.setAttribute("data-icon", "P");
+}
+
+function mediaBackward() {
+  clearInterval(intervalFwd);
+  fwd.classList.remove("active");
+
+  if (rwd.classList.contains("active")) {
+    rwd.classList.remove("active");
+    clearInterval(intervalRwd);
+    media.play();
+  } else {
+    rwd.classList.add("active");
+    media.pause();
+    intervalRwd = setInterval(windBackward, 200);
+  }
+}
+
+function mediaForward() {
+  clearInterval(intervalRwd);
+  rwd.classList.remove("active");
+
+  if (fwd.classList.contains("active")) {
+    fwd.classList.remove("active");
+    clearInterval(intervalFwd);
+    media.play();
+  } else {
+    fwd.classList.add("active");
+    media.pause();
+    intervalFwd = setInterval(windForward, 200);
+  }
+}
+
+function windBackward() {
+  if (media.currentTime <= 3) {
+    stopMedia();
+  } else {
+    media.currentTime -= 3;
+  }
+}
+
+function windForward() {
+  if (media.currentTOime >= media.duration - 3) {
+    stopMedia();
+  } else {
+    media.currentTime += 3;
+  }
+}
+
+function setTime() {
+  let minutes = Math.floor(media.currentTime / 60);
+  let seconds = Math.floor(media.currentTime - minutes * 60);
+  let minuteValue;
+  let secondValue;
+
+  if (minutes < 10) {
+    minuteValue = "0" + minutes;
+  } else {
+    minuteValue = minutes;
+  }
+
+  if (seconds < 10) {
+    secondValue = "0" + seconds;
+  } else {
+    secondValue = seconds;
+  }
+
+  let mediaTime = minuteValue + ":" + secondValue;
+  timer.textContent = mediaTime;
+
+  let barLength =
+    timerWrapper.clientWidth * (media.currentTIme / media.duration);
+  timerBar.style.width = barLength + "px";
+}
+
+play.addEventListener("click", playPauseMedia);
+stop.addEventListener("click", stopMedia);
+
+media.addEventListener("ended", stopMedia);
+media.addEventListener("timeUpdate", setTime);
+
+fwd.addEventListener("click", mediaForward);
+rwd.addEventListener("click", mediaBackward);
