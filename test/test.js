114c930a97345c636599f3497b55ad95f96c5baf
Josh Dorn <jr_dorn@yahoo.com>
2021-07-08 23:29:10 -0500

    Modal image

--- test/test.js
+++ test/test.js
@@ -1,14 +1,26 @@
+// Doc elements
 const modalBtn = document.querySelector("#modalBtn");
 const modalA = document.querySelector("#modalA");
-const modalAClose = document.querySelector("#modalAClose");
+const modalAClose = document.querySelector(".modalClose");
+const modImg = document.querySelector("#modImg");
+const modImg2 = document.querySelector("#modImg2");
 
+// Event listeners
 modalBtn.addEventListener("click", modalClickHandler);
 modalAClose.addEventListener("click", modalCloseHandler);
+modImg.addEventListener("click", modImgHandler);
 
+// Functions
 function modalClickHandler() {
   modalA.classList.remove("hidden");
 }
 
 function modalCloseHandler() {
   modalA.classList.add("hidden");
+  console.log(1);
+}
+
+function modImgHandler() {
+  modImg2.classList.remove("hidden");
+  modImg2.style.display = "block";
 }
