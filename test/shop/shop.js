80cbaa35e0317d76bc82a4e74d0e4cfcefab7e46
Josh Dorn <jr_dorn@yahoo.com>
2021-07-18 13:21:05 -0500

    Side navbar markup

--- test/shop/shop.js
+++ test/shop/shop.js
@@ -3,10 +3,20 @@
 const ham = document.querySelector(".ham");
 const navMenu = document.querySelector(".navMenu");
 
-//
+//toggle hamburger menu
 function mobileMenu() {
   ham.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
 
 ham.addEventListener("click", mobileMenu);
+
+//close hamburger menu when link clicked
+const navLink = document.querySelectorAll(".navLink");
+
+navLink.forEach((link) => link.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  ham.classList.remove("active");
+  navMenu.classList.remove("active");
+}
