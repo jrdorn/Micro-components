c435eed0db59d77c4487220b147053fedca0a1bc
Josh Dorn <jr_dorn@yahoo.com>
2021-07-25 23:27:33 -0500

    Square module

--- cluster/app.js
+++ cluster/app.js
@@ -4,6 +4,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//
+const square = require("./square");
+
 //route definition
 //callback function that is invoked whenever there is an HTTP GET request
 //with a path relative to the site root
@@ -14,4 +17,7 @@ app.get("/", (req, res) => {
 //start the server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+
+  //
+  console.log("The area of a square with a width of 5 is " + square.area(5));
 });
